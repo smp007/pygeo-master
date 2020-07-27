440bba167fb1209418d27c11d4f45463c22138de
Sudarsan <sudarsanmpai.1@gmail.com>
2020-07-27 20:09:37 +0200

    Intersect Function implemented

--- src/pygeo/intersect.py
+++ src/pygeo/intersect.py
@@ -1,13 +1,44 @@
-from .objects import Ray, Sphere, Triangle
+from pygeo.objects import Ray, Sphere, Triangle, Point, Vector
+import numpy as np
 
 
 def intersect(first_object, second_object):
     ...
 
 
-def _intersect_ray_with_sphere(ray, sphere):
-    ...
+def _intersect_ray_with_sphere(ray, sphere):    
+
+    o = ray._origin
+    u = ray._direction
+    r = sphere._radius
+    c = sphere._center
+    norm_u = np.linalg.norm(u._vector)
+    U = (1/norm_u) * u._vector
+
+    b = 2 * np.dot(u._vector,(o-c)._vector)
+    a = 1
+    c = np.dot((o-c)._vector,(o-c)._vector) - r**2
+    discriminant = b**2 - 4*a*c
+    
+
+    if discriminant > 0:
+        square_root_discriminant = np.sqrt(discriminant)
+        d1 = (-1*b + square_root_discriminant)/ (2*a)
+        d2 = (-1*b - square_root_discriminant)/ (2*a)        
+        p1 = Point(o._point + (d1 * U))
+        p2 = Point(o._point + (d2 * U))
+        return p1,p2
+        
+    elif discriminant == 0:
+        d = -1*b / 2*a
+        p = Point(o._point +(d*U))
+        return p
+
+    else:
+        return "Does not intersect"
+
 
 
 def _intersect_ray_with_triangle(ray, triangle):
     ...
+
